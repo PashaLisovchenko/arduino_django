5b771a26007784a0815a22fa1935fec32e6561d1
pasha <lisovchenko.pasha@gmail.com>
2017-12-12 21:03:11 +0200

    add post, form_valid methods

--- boards/forms.py
+++ boards/forms.py
@@ -12,12 +12,12 @@ STAKEHOLDERS_CHOICES = [(i.lower(), i) for i in STAKEHOLDERS]
 class RequirementsForm(forms.Form):
     knowledge_level = forms.ChoiceField(choices=STAKEHOLDERS_CHOICES)
 
-    analog = forms.IntegerField(min_value=1)
-    digit = forms.IntegerField(min_value=1)
-    voltage = forms.DecimalField(min_value=1, max_value=10)
-    processor_family = forms.ModelChoiceField(queryset=family_obj)
+    analog = forms.IntegerField(min_value=1, required=False)
+    digit = forms.IntegerField(min_value=1, required=False)
+    voltage = forms.DecimalField(min_value=1, max_value=10, required=False)
+    processor_family = forms.ModelChoiceField(queryset=family_obj, required=False)
 
-    language = forms.ModelChoiceField(queryset=language_obj)
+    language = forms.ModelChoiceField(queryset=language_obj, required=False)
 
-    price = forms.DecimalField(min_value=0, decimal_places=2)
-    form = forms.IntegerField()
+    price = forms.DecimalField(min_value=0, decimal_places=2, required=False)
+    form = forms.IntegerField(required=False)
--- boards/views.py
+++ boards/views.py
@@ -1,24 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import FormMixin
 from .models import Board, Category
 from django.views.generic import ListView, DetailView
 from .forms import RequirementsForm
 
 
-class BoardList(ListView):
+class BoardList(FormMixin, ListView):
     template_name = 'boards/list.html'
     model = Board
+    form_class = RequirementsForm
     paginate_by = 9
     context_object_name = 'boards'
 
     def get_context_data(self, **kwargs):
         context = super(BoardList, self).get_context_data(**kwargs)
         category = None
-        form = RequirementsForm()
-        context['form'] = form
         categories = Category.objects.all()
         context['category'] = category
         context['categories'] = categories
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        print(self.request.POST)
+        req = self.request.POST
+        if not req['processor_family']:
+            print("*"*50)
+        return redirect('boards:board_list')
 
 
 class BoardListByCategory(DetailView):
