e84c249dba09ce1ffaef8651813cf4beb848e1da
pasha <lisovchenko.pasha@gmail.com>
2017-12-24 12:11:15 +0200

    fix forms(voltage, form-factor)

--- boards/forms.py
+++ boards/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import Languages, FamilyProcessor
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.fields import BLANK_CHOICE_DASH
+
 
 language_obj = Languages.objects.all()
 family_obj = FamilyProcessor.objects.all()
@@ -8,8 +10,11 @@ family_obj = FamilyProcessor.objects.all()
 STAKEHOLDERS = [_('Beginner'), _('Professional')]
 STAKEHOLDERS_CHOICES = [(i.lower(), i) for i in STAKEHOLDERS]
 
-FORM_FACTOR = ['', _('Small'), _('Medium'), _('Large')]
-FORM_FACTOR_CHOICES = [(i.lower(), i) for i in FORM_FACTOR]
+FORM_FACTOR = [_('Small'), _('Medium'), _('Large')]
+FORM_FACTOR_CHOICES = BLANK_CHOICE_DASH+[(i.lower(), i) for i in FORM_FACTOR]
+
+VOLTAGE = ['3.3', '5', '12']
+VOLTAGE_CHOICES = BLANK_CHOICE_DASH+[(i.lower(), i) for i in VOLTAGE]
 
 
 class RequirementsForm(forms.Form):
@@ -17,7 +22,7 @@ class RequirementsForm(forms.Form):
 
     analog = forms.IntegerField(min_value=1, required=False)
     digit = forms.IntegerField(min_value=1, required=False)
-    voltage = forms.DecimalField(min_value=1, max_value=10, required=False)
+    voltage = forms.ChoiceField(choices=VOLTAGE_CHOICES, required=False)
     processor_family = forms.ModelChoiceField(queryset=family_obj, required=False)
 
     language = forms.ModelChoiceField(queryset=language_obj, required=False)
