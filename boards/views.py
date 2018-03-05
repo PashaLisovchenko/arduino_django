7eb7a041ab446c36106df434d7b7337bcd38e680
pasha <lisovchenko.pasha@gmail.com>
2018-03-05 22:33:43 +0200

    UPDATE: global update

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic.edit import FormMixin
 from .models import Board, Category
@@ -36,11 +37,13 @@ def get_board(req, object_list, request, form_class, category, categories):
     kl = boards
     query_response = []
 
+    boards = get_distance_boards(kl, analog=req['analog'], digit=req['digit'],
+                                 voltage=req['voltage'], price=req['price'])
     if req['processor_family']:
         boards = boards.filter(processor__family_id=req['processor_family'])
         query_response.append('processor_family')
     if req['language']:
-        boards = boards.filter(programming_languages__id = req['language'])
+        boards = boards.filter(programming_languages__id=req['language'])
         query_response.append('language')
     if req['form']:
         boards = get_board_form(req, boards)
@@ -48,40 +51,42 @@ def get_board(req, object_list, request, form_class, category, categories):
 
     if boards:
         # после сужения, платы есть
-        if len(boards) == 1:
-            # вывод платы если она одна
-            return render(request, 'boards/list.html', context={'boards': boards, 'form': form_class,
-                                                                'category': category, 'categories': categories,
-                                                                'rec': True})
-        elif len(boards) > 1:
-            boards = get_distance_boards(boards, analog=req['analog'], digit=req['digit'],
-                                         voltage=req['voltage'], price=req['price'])
-            return render(request, 'boards/list.html', context={'boards': boards, 'form': form_class,
-                                                                'category': category, 'categories': categories,
-                                                                'rec': True})
+        if len(boards) > 1:
+            boards = boards[:4]
+        return render(request, 'boards/recommendations.html', {'boards': boards, 'form': form_class,
+                               'category': category, 'categories': categories, 'rec': True})
     else:
-        # todo узнать искать среди всех, или только по уровню знаний?
+        # искать среди всех, тогда либо исключать найденные лучшие платы по другим критериям,
+        # либо делать множества и определять пересечения и определять лучшие по нескольким критериям одновременно
         ol = object_list.all()
+        boards = get_distance_boards(ol, analog=req['analog'], digit=req['digit'],
+                                     voltage=req['voltage'], price=req['price'])
         # после сужения плат нету, ищем лучшие результаты по категориям отдельно
         if 'processor_family' in query_response:
-            boards_processor = ol.filter(processor__family_id=req['processor_family'])
-            boards_processor = get_distance_boards(boards_processor, analog=req['analog'],
-                                                   digit=req['digit'], voltage=req['voltage'], price=req['price'])
+            boards_processor = boards.filter(processor__family_id=req['processor_family'])
+            # boards_processor = get_distance_boards(boards_processor, analog=req['analog'],
+            #                                        digit=req['digit'], voltage=req['voltage'], price=req['price'])
         else:
             boards_processor = None
         if 'language' in query_response:
-            boards_language = ol.filter(programming_languages__id=req['language'])
-            boards_language = get_distance_boards(boards_language, analog=req['analog'],
-                                                  digit=req['digit'], voltage=req['voltage'], price=req['price'])
+            boards_language = boards.filter(programming_languages__id=req['language'])
+            if boards_processor:
+                boards_language = boards_language.exclude(id=boards_processor[0].id)
+            # boards_language = get_distance_boards(boards_language, analog=req['analog'],
+            #                                       digit=req['digit'], voltage=req['voltage'], price=req['price'])
         else:
             boards_language = None
         if 'form' in query_response:
-            boards_form = get_board_form(req, ol)
-            boards_form = get_distance_boards(boards_form, analog=req['analog'], digit=req['digit'],
-                                              voltage=req['voltage'], price=req['price'])
+            boards_form = get_board_form(req, boards)
+            if boards_processor:
+                boards_form = boards_form.exclude(id=boards_processor[0].id)
+            if boards_language:
+                boards_form = boards_form.exclude(id=boards_language[0].id)
+            # boards_form = get_distance_boards(boards_form, analog=req['analog'], digit=req['digit'],
+            #                                   voltage=req['voltage'], price=req['price'])
         else:
             boards_form = None
-        return render(request, 'boards/list.html',
+        return render(request, 'boards/recommendations.html',
                       context={'boards_form': boards_form, 'boards_processor': boards_processor,
                                'boards_language': boards_language, 'form': form_class, 'category': category,
                                'categories': categories, 'rec': True
@@ -90,16 +95,20 @@ def get_board(req, object_list, request, form_class, category, categories):
 
 def get_distance_boards(boards, analog=None, digit=None, voltage=None, price=None):
     if not analog and not digit and not voltage and not price:
-        return boards[:4]
+        return boards
     else:
+        # todo рекомендовать ли платы с количеством меньше чем ввел пользователь?
+        # сделать критерии, продумать формулу для росчета веса критерия
         if analog:
-            boards = boards.annotate(d_analog=ExpressionWrapper(
+            # filter(analog_port__gt=0)
+            # filter(analog_port__gte=int(analog))
+            boards = boards.filter(analog_port__gte=int(analog)).annotate(d_analog=ExpressionWrapper(
                     (int(analog) - F('analog_port')) * (int(analog) - F('analog_port')),
                     output_field=DecimalField()))
         else:
             boards = boards.annotate(d_analog=ExpressionWrapper(Value(0), output_field=DecimalField()))
         if digit:
-            boards = boards.annotate(d_digit=ExpressionWrapper(
+            boards = boards.filter(digital_port__gt=0).annotate(d_digit=ExpressionWrapper(
                 (int(digit) - F('digital_port')) * (int(digit) - F('digital_port')),
                 output_field=DecimalField()))
         else:
@@ -111,8 +120,11 @@ def get_distance_boards(boards, analog=None, digit=None, voltage=None, price=Non
         else:
             boards = boards.annotate(d_voltage=ExpressionWrapper(Value(0), output_field=DecimalField()))
         if price:
+            # max - min / 2 = a
+            # max - a = avg
             boards = boards.annotate(d_price=ExpressionWrapper(
-                (Decimal(price) - F('max_price')) * (Decimal(price) - F('max_price')),
+                (Decimal(price) - (F('max_price') - ((F('max_price')-F('min_price'))/2))) *
+                (Decimal(price) - (F('max_price') - ((F('max_price')-F('min_price'))/2))),
                 output_field=DecimalField()))
         else:
             boards = boards.annotate(d_price=ExpressionWrapper(Value(0), output_field=DecimalField()))
@@ -120,7 +132,8 @@ def get_distance_boards(boards, analog=None, digit=None, voltage=None, price=Non
         boards = boards.annotate(expires=ExpressionWrapper(
             F('d_analog') + F('d_digit') + F('d_voltage') + F('d_price'),
             output_field=DecimalField())).order_by('expires')
-        return boards[:4]
+        print(boards)
+        return boards
 
 
 class BoardList(FormMixin, ListView):
@@ -130,6 +143,29 @@ class BoardList(FormMixin, ListView):
     paginate_by = 9
     context_object_name = 'boards'
 
+    def get(self, request, *args, **kwargs):
+        if self.request.is_ajax():
+            req = request.GET
+            object_list = Board.objects.all()
+            category = None
+            categories = Category.objects.all()
+            return get_board(req, object_list, self.request, self.form_class, category, categories)
+        else:
+            self.object_list = self.get_queryset()
+            allow_empty = self.get_allow_empty()
+
+            if not allow_empty:
+                if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
+                    is_empty = not self.object_list.exists()
+                else:
+                    is_empty = len(self.object_list) == 0
+                if is_empty:
+                    raise Http404(("Empty list and '%(class_name)s.allow_empty' is False.") % {
+                        'class_name': self.__class__.__name__,
+                    })
+            context = self.get_context_data()
+            return self.render_to_response(context)
+
     def get_context_data(self, **kwargs):
         context = super(BoardList, self).get_context_data(**kwargs)
         category = None
@@ -138,20 +174,21 @@ class BoardList(FormMixin, ListView):
         context['categories'] = categories
         return context
 
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
-
-    def form_valid(self, form):
-        print(self.request.POST)
-        req = self.request.POST
-        all_boards = Board.objects.all()
-        category = None
-        categories = Category.objects.all()
-        return get_board(req, all_boards, self.request, self.form_class, category, categories)
+    # def post(self, request, *args, **kwargs):
+    #     print(self.request.is_ajax())
+    #     form = self.get_form()
+    #     if form.is_valid():
+    #         return self.form_valid(form)
+    #     else:
+    #         return self.form_invalid(form)
+    #
+    # def form_valid(self, form):
+    #     print(self.request.POST)
+    #     req = self.request.POST
+    #     object_list = Board.objects.all()
+    #     category = None
+    #     categories = Category.objects.all()
+    #     return get_board(req, object_list, self.request, self.form_class, category, categories)
 
 
 class BoardListByCategory(FormMixin, DetailView):
@@ -170,10 +207,8 @@ class BoardListByCategory(FormMixin, DetailView):
         try:
             boards = paginator.page(page)
         except PageNotAnInteger:
-            # If page is not an integer deliver the first page
             boards = paginator.page(1)
         except EmptyPage:
-            # If page is out of range deliver last page of results
             boards = paginator.page(paginator.num_pages)
 
         context['page'] = page
@@ -181,20 +216,33 @@ class BoardListByCategory(FormMixin, DetailView):
         context['boards'] = boards
         return context
 
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+    def get(self, request, *args, **kwargs):
+        if self.request.is_ajax():
+            req = request.GET
+            category = self.get_object()
+            category_boards = Board.objects.filter(category=category)
+            categories = Category.objects.all()
+            return get_board(req, category_boards, self.request, self.form_class, category, categories)
 
-    def form_valid(self, form):
-        print(self.request.POST)
-        category = self.get_object()
-        category_boards = Board.objects.filter(category=category)
-        req = self.request.POST
-        categories = Category.objects.all()
-        return get_board(req, category_boards, self.request, self.form_class, category, categories)
+        else:
+            self.object = self.get_object()
+            context = self.get_context_data(object=self.object)
+            return self.render_to_response(context)
+
+    # def post(self, request, *args, **kwargs):
+    #     form = self.get_form()
+    #     if form.is_valid():
+    #         return self.form_valid(form)
+    #     else:
+    #         return self.form_invalid(form)
+    #
+    # def form_valid(self, form):
+    #     print(self.request.POST)
+    #     category = self.get_object()
+    #     category_boards = Board.objects.filter(category=category)
+    #     req = self.request.POST
+    #     categories = Category.objects.all()
+    #     return get_board(req, category_boards, self.request, self.form_class, category, categories)
 
 
 class BoardDetail(DetailView):
