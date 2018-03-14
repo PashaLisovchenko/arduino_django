432afd60f2b7fbd5614d6b73f0f7a9b16e830cc6
PashaLisovchenko <lisovchenko.pasha@gmail.com>
2018-03-14 16:36:52 +0200

    UPDADE: redesign2

--- boards/static/js/ajax_recomended_boards.js
+++ boards/static/js/ajax_recomended_boards.js
@@ -1,26 +1,3 @@
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    }
-});
 $(document).ready(function() {
     var url = document.location.href;
     $('.send').click(function() {
@@ -51,15 +28,15 @@ $(document).ready(function() {
                 'price':price,
                 'form':form
             },
-            success: updateMessage,
+            success: recommendation,
             failure: function (data) {
                 console.log("FAIL");
                 console.log(data);
             }
         });
-        function updateMessage(data) {
+        function recommendation(data) {
             console.log("SUCCESS");
-            console.log(data);
+            $('#boards').css({display: 'none'});
             $('.board-tags').css({display: 'none'});
             $('.recommend').html(data);
         }
